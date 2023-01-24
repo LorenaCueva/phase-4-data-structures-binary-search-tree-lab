dfc0cd3a79d9d9f0a1f523911a8584a5d6cdf50b
Lorena Cueva <lorenacueva@gmail.com>
2023-01-23 16:07:34 -0800

    commit tree d867e0f9ad6c518d539ea83d837b575375c548ad

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,44 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    return nil if @root.nil?
+
+    curr = @root
+
+    while curr
+      return curr if curr.value == value
+      if value < curr.value
+        curr = curr.left
+      else
+        curr = curr.right
+      end
+      nil
+    end
   end
 
   def insert(value)
-    # your code here
+    new_node = Node.new(value)
+    if @root.nil?
+      @root = new_node
+      return @root
+    end
+
+    curr = @root
+
+    while curr
+      return nil if curr.value == value
+      if value < curr.value
+        if curr.left.nil?
+          curr.left = new_node
+          return new_node
+        end
+        curr = curr.left
+      elsif curr.right.nil?
+          curr.right = new_node
+          return new_node
+      end
+         curr = curr.right
+    end 
   end
 
 end 
